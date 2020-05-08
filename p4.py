3a801b5bda0bc278b7799401f343533fca3bd3b3
srujan <srujans321@gmail.com>
2020-05-08 20:55:55 +0530

    with objects

--- p4.py
+++ p4.py
@@ -1,10 +1,31 @@
 import numpy as np
 #inputs are batches otherwise called list of lists
 
-inputs = [[1, 2, 3, 2.5],
+np.random.seed(0)
+
+X = [[1, 2, 3, 2.5],
            [2.0, 5.0, -1.0, 2.0],
            [-1.5, 2.7, 3.3, -0.8]]
 
+# 2 hidden layers
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+#print(0.10*np.random.randn(4,3))
+
+layer1 = Layer_Dense(4, 5) # the 5 number here can be any number
+layer2 = Layer_Dense(5, 6)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
+
+
+"""
 weights = [[0.2, 0.8, -0.5, 1.0],
             [0.5, -0.91, 0.26, -0.5],
             [-0.26, -0.27, 0.17, 0.87]]
@@ -27,4 +48,4 @@ print(layer2_outputs)
 # this goes on and on, so to avoid this problem we're moving python objects
 
 
-
+"""
